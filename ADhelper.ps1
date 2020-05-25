600625ebbbab33cfb11871c0dd29ad89bf8a372c
Nathan McBride <nathan.mcbride@thecronosgroup.com>
2020-05-25 14:32:18 -0400

    Created git repo

---
+++ ADhelper.ps1
@@ -0,0 +1,56 @@
+
+
+$CurrentUser = $(whoami.exe)
+[bool]$IsDomainAdmin = (Get-ADUser $CurrentUser -Properties memberof).memberof -contains (Get-ADGroup "Domain Admins")  
+
+if($IsDomainAdmin) {
+    MainMenu($CurrentUser)
+}
+else {
+    Write-Host "You must run this program as a Domain Administrator" -ForegroundColor Red
+    Exit
+}
+
+function MainMenu {
+    param ( $CurrentUser )
+    [bool]$Quit = $false
+    while ( -not $Quit ) {
+        $input = Read-Host "[ $CurrentUser ] $ "
+
+        switch ($input) {
+            1 { }
+            2 { }
+            3 { }
+            Default { help(); }
+        }
+        
+
+    }
+}
+
+function help {
+
+}
+
+function unlock {
+    param ( $UserName )
+    # Search for a locked out account matching the name provided by the user.
+    $ADaccount = Search-ADAccount -LockedOut | Where-Object { $_.SamAccountName -match $UserName }
+    if( ($ADaccount) ) {
+        Unlock-ADAccount -Identity $ADaccount
+        Write-Host $ADaccount.SamAccountName "unlocked sucessfuly" -ForegroundColor Green
+    }
+    # Account does not exist or is not locked.
+    else {   
+        Write-Host "$UserName not listed as a locked account..." -ForegroundColor Red
+    }
+}
+
+function checkExpire {
+    param ( $DaysUntilExpired )
+
+    Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" |
+    Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} |
+    Where-Object ($_.ExpiryDate -lt )
+
+}
